a096020727cf8f14de2b570786b3179b44ba6211
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-04-24 20:32:57 +0530

    Update variables.tf

--- ec2_instance/variables.tf
+++ ec2_instance/variables.tf
@@ -1,5 +1,12 @@
-variable "region" {}
-variable "ami_id" {}
-variable "instance_type" {}
-variable "key_name" {}
-variable "private_key_path" {}
+variable "region" {
+    default= "us-east-1"
+}
+variable "ami_id" {
+    default= "ami-075686beab831bb7f"
+}
+variable "instance_type" {
+    default= "t2.micro"
+}
+variable "key_name" {
+    default="lavanya-github-key"
+}
