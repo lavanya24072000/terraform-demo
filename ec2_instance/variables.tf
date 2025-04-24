7a538154b9616ab093adcd249e0c6210afe7bc5e
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-04-24 20:44:26 +0530

    Update variables.tf

--- ec2_instance/variables.tf
+++ ec2_instance/variables.tf
@@ -2,7 +2,7 @@ variable "region" {
     default= "us-east-1"
 }
 variable "ami_id" {
-    default= "ami-075686beab831bb7f"
+    default= "ami-084568db4383264d4"
 }
 variable "instance_type" {
     default= "t2.micro"
