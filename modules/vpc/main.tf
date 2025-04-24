0b5a73eac9f6ca679edd23610416acb5ea553555
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-04-24 22:40:58 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -23,6 +23,16 @@ resource "aws_subnet" "public" {
     Name = "public-subnet-${count.index + 1}"
   }
 }
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = "us-east-1b"
+ 
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+}
  
  
 resource "aws_route_table" "public" {
