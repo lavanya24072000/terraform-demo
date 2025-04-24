2fc9179dad900a983ecc75cabb649ba0e3776deb
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-04-24 21:02:24 +0530

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -9,6 +9,25 @@ resource "aws_instance" "bastion" {
   tags = {
     Name = "Terraform-Bastion"
   }
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion-sg"
+  description = "Allow SSH access"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # open for testing
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+
+}
  
   provisioner "file" {
     source      = "../network_infra"
