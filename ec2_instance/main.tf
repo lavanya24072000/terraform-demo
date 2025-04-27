43d6713679035bfcdb2c1d1b42ac66e1d9d19a42
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-04-27 12:32:43 +0530

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -46,11 +46,11 @@ resource "aws_instance" "bastion" {
   }
 }
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "bastion-instance-profile-12"
+  name = "bastion-instance-profile-13"
   role = "dummy"
 }
 resource "aws_security_group" "bastion_sg" {
-  name        = "bastion-sg18"
+  name        = "bastion-sg19"
   description = "Allow SSH access"
   ingress {
     from_port   = 22
