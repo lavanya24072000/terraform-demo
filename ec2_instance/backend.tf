6b18fda35602c74822f5257141a70f8a21fa266a
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-04-27 15:40:56 +0530

    Create backend.tf

---
+++ ec2_instance/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "lavanya-bucket001"
+    key    = "terraform/state/network_infra.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true 
+    
+  }
+}
