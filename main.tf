7d9c4b02c37bb61c96fa7e3f7987a06c4de6faa6
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-04-27 16:43:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ provider "aws"{
  region="us-east-1"
 
 }
-resource "aws_instance""my_ec2"{
-
-
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-084568db4383264d4",
+  instance_type = "t2.micro"
+ 
 }
